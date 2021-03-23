052fad00a0f3d9bad6d6a29aa8fba04eff486725
Jake Webster <nocryingoverburnttoast@gmail.com>
2021-03-22 20:17:06 -0400

    added ifs and fors into templates

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from datetime import datetime
 from model import db
 
@@ -11,12 +11,18 @@ counter = 0
 def welcome():
     increment_view_count()
     return render_template("welcome.html",
-    message="Catch them donkeys")
+    cards=db)
 
-@app.route("/card")
-def cards():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def cards(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                                 card=card,
+                                 index=index,
+                                 max_index=len(db)-1)
+    except IndexError:
+        abort(404)
 
 @app.route("/date")
 def date():
